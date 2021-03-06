7f1f80f6dcfff9b10556f283a3494988336d3f76
calebe <calebe.fazzani.facul@gmail.com>
2021-03-06 10:43:43 -0300

    #397 - Adição de service

--- src/main/java/hyve/petshow/service/port/NegociacaoService.java
+++ src/main/java/hyve/petshow/service/port/NegociacaoService.java
@@ -3,12 +3,16 @@ package hyve.petshow.service.port;
 import org.springframework.stereotype.Service;
 
 import hyve.petshow.domain.Negociacao;
+import hyve.petshow.exceptions.BusinessException;
+import hyve.petshow.exceptions.NotFoundException;
 
 @Service
 public interface NegociacaoService {
-	Negociacao criaNegociacao(Long idAgendamento, Negociacao negociacao);
+	Negociacao criaNegociacao(Long idAgendamento, Long idDono, Negociacao negociacao) throws BusinessException, NotFoundException;
 
-	Negociacao atualizaNegociacao(Long idNegociacao, Negociacao negociacao);
+	Negociacao atualizaNegociacao(Long idNegociacao, Negociacao negociacao) throws BusinessException, NotFoundException;
 
-	Negociacao cancelaNegociacao(Long idNegociacao);
+	Negociacao cancelaNegociacao(Long idNegociacao) throws BusinessException, NotFoundException;
+	
+	Negociacao buscaPorId(Long idNegociacao) throws NotFoundException;
 }
