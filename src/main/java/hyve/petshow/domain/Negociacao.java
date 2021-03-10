20e7330251adb952186fba593987a4e15b714c08
gaugsouza <guilhermeguis@outlook.com>
2021-03-09 23:42:44 -0300

    ajustes importantes

--- src/main/java/hyve/petshow/domain/Negociacao.java
+++ src/main/java/hyve/petshow/domain/Negociacao.java
@@ -1,16 +1,10 @@
 package hyve.petshow.domain;
-import java.math.BigDecimal;
-
-import javax.persistence.Column;
-import javax.persistence.Embedded;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
 import hyve.petshow.domain.embeddables.Auditoria;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
 
 @Entity
 @Data
@@ -20,7 +14,7 @@ public class Negociacao {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private BigDecimal precoInicial;
-	private BigDecimal precoOferta;
+	private BigDecimal precoOfertado;
 	private String respostaOferta;
 	@Column(name = "fk_agendamento")
 	private Long idAgendamento;
